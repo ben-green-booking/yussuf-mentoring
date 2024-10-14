49d40ddf2557f752b29c87a210d106c5678c36ff
Yussuf Shariff <yussuf.shariff@booking.com>
2024-10-14 16:28:38 +0100

    v1 endpoints converted to spring endpoints

--- part-7-spring-boot/src/main/java/com/mentoring/yussuf/PetShopApplication.java
+++ part-7-spring-boot/src/main/java/com/mentoring/yussuf/PetShopApplication.java
@@ -1,8 +1,12 @@
 package com.mentoring.yussuf;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetShopApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopApplication.class, args);
+    }
 }
--- part-7-spring-boot/src/main/java/com/mentoring/yussuf/v1/controller/PetShopController.java
+++ part-7-spring-boot/src/main/java/com/mentoring/yussuf/v1/controller/PetShopController.java
@@ -3,12 +3,13 @@ package com.mentoring.yussuf.v1.controller;
 import com.mentoring.yussuf.entity.Pet;
 import com.mentoring.yussuf.service.PetShopService;
 import com.mentoring.yussuf.v1.dto.*;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("v1/pets")
 public class PetShopController {
 
     private final PetShopService petShopService;
@@ -17,7 +18,8 @@ public class PetShopController {
         this.petShopService = petShopService;
     }
 
-    public int createPet(CreatePetDTO createPetDTO) {
+    @PostMapping
+    public int createPet(@RequestBody CreatePetDTO createPetDTO) {
         if (createPetDTO.species() == null) {
             throw new RuntimeException("Species is a mandatory field for a new pet");
         }
@@ -29,19 +31,24 @@ public class PetShopController {
                 createPetDTO.species()).getId();
     }
 
-    public void updatePet(UpdatePetDTO updatePetDTO) {
+    @PatchMapping("/{id}")
+    public void updatePet(@RequestBody UpdatePetDTO updatePetDTO) {
         petShopService.updatePet(updatePetDTO.id(), updatePetDTO.age(), updatePetDTO.price(), updatePetDTO.sold());
+        // Have to manually put id in on postman instead of taking from URL, URL or Request Body?
     }
 
-    public void deletePet(int petId) {
+    @DeleteMapping
+    public void deletePet(@PathVariable int petId) {
         petShopService.deletePet(petId);
     }
 
-    public List<GetPetDTO> getPetsBy(String species, boolean availableOnly) {
+    @GetMapping
+    public List<GetPetDTO> getPetsBy(@RequestParam(required = false) String species, @RequestParam boolean availableOnly) {
         return petShopService.getPetsBy(species, availableOnly).stream().map(this::toGetPetDTO).toList();
     }
 
-    public Optional<GetPetDTO> getPetById(int id) {
+    @RequestMapping("/{id}")
+    public Optional<GetPetDTO> getPetById(@PathVariable int id) {
         return petShopService.getPetById(id).map(this::toGetPetDTO);
     }
 
